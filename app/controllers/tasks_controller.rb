a3c817d43570f1c883125a44da5663ffa97ca566
eyal gerby <eyalg535@gmail.com>
2023-02-16 15:51:08 +0200

    create tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+  
+    get '/tasks' do
+      tasks = Task.all.order(:category_id)
+      tasks.to_json
+    end
+  
+    get '/tasks/:id' do
+      task = Task.find(params[:id])
+      task.to_json
+    end
+  
+    post '/tasks' do
+      task = Task.new(params[:task])
+      if task.save
+        task.to_json
+      else
+        { errors: task.errors.full_messages }.to_json
+      end
+    end
+  
+    patch '/tasks/:id' do
+      task = Task.find(params[:id])
+      if task && task.update(params[:task])
+        task.to_json
+      else
+        { errors: task.errors.full_messages }.to_json
+      end
+    end
+  
+    delete '/tasks/:id' do
+      task = Task.find(params[:id])
+      task.destroy
+      task.to_json
+    end
+  end
