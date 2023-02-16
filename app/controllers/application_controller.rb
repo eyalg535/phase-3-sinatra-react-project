c070d9fc555a0d0d04410a2aa0492ff91de9a240
eyal gerby <eyalg535@gmail.com>
2023-02-16 15:49:33 +0200

    updates app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,3 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/tasks" do
-    tasks = Task.all.order(:category_id)
-    tasks.to_json
-  end
-
-  get "/tasks/:id" do
-    task = Task.find(params[:id])
-    task.to_json
-  end
-
-  post "/tasks" do
-    task = Task.new(params[:task])
-    if task.save
-      task.to_json
-    else
-      { errors: task.errors.full_messages }.to_json
-    end 
-  end
-
 end
